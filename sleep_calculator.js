bc401293cfc711556ef6ad55c44dec18e86360dd
Liz U <117729816+Sadeyes57@users.noreply.github.com>
2023-04-07 23:35:14 +0100

    Add files via upload
    A calculator that works out how much sleep you are getting. Is it too little or too much?

---
+++ sleep_calculator.js
@@ -0,0 +1,9 @@
+num = input("Please enter a number: ")
+
+while (num > 0)
+  print(num)
+  if (num < 0)
+  
+  input_list = []
+  num_list = (input_list.append(num()) / len(input_list))
+  print(str(num_list))
